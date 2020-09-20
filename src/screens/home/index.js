f980063fbabf2242ff7c364265c72fd12506320e
AndreSDS <assilva4@stefanini.com>
2020-09-20 15:00:12 -0300

    iniciando lista horizontal de imagens

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Wrapper,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+  GiftDiscount,
+} from "./styles";
+
+import Suggestions from "../../components/Suggestions";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HOME</Text>
-    </View>
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c86e"
+          />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <GiftDiscount>
+            <AntDesign name="gift" size={30} color="#10c86e" />
+            <MaterialCommunityIcons
+              name="ticket-percent"
+              style={{ transform: [{ rotate: "90deg" }] }}
+              size={30}
+              color="#10c86e"
+            />
+          </GiftDiscount>
+        </Header>
+
+        <Suggestions></Suggestions>
+      </Container>
+    </Wrapper>
   );
 };
 
